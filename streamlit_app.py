1da7481f275ff2cccf8adfda15e73ec59a113966
mcvrmln <103961469+mcvrmln@users.noreply.github.com>
2023-03-23 22:20:29 +0100

    Pandas response
    Added pandas response displayed

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# transform response to a pandas dataframe 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Show the pandas dataframe in Streamlit
+streamlit.dataframe(fruityvice_normalized)
