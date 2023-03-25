0daeca48d13521c17a700cdb6195e57bdabd80e2
mcvrmln <103961469+mcvrmln@users.noreply.github.com>
2023-03-25 21:21:06 +0100

    Removed stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,4 +66,3 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
-streamlit.stop()
